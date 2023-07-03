083095169b2ff45ceea832aad4960df64aa2000e
Donghee Nam <57467269+dongheenam@users.noreply.github.com>
2023-07-03 18:16:00 +1000

    Fix TypeScript error

--- src/routes/edit/EditCourseGroup.svelte
+++ src/routes/edit/EditCourseGroup.svelte
@@ -1,14 +1,33 @@
 <script lang="ts">
   import type { CourseGroup } from '$lib/types';
 
+  import EditableTable from '$lib/components/EditableTable.svelte';
+
   export let courseGroup: CourseGroup | undefined;
+
+  // TODO: fix TypeScript error
+  const COLUMNS: {
+    header: string;
+    key: 'code' | 'staffCode';
+    editable: boolean;
+  }[] = [
+    { header: 'Class', key: 'code', editable: true },
+    { header: 'Staff', key: 'staffCode', editable: true },
+  ];
 </script>
 
 <div>
   {#if courseGroup}
     <h3>Classes in {courseGroup.name}</h3>
     <input bind:value={courseGroup.name} />
+    <EditableTable
+      bind:items={courseGroup.courses}
+      columns={COLUMNS}
+      selectable
+      addItem={() => ({ code: '', staffCode: '' })}
+    />
   {:else}
     <h3>Create new group</h3>
+    <p>(coming soon...)</p>
   {/if}
 </div>
